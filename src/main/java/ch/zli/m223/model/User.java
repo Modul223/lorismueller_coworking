027f2615d484948b728cc239312e4bb2b1db5901
LorisMueller <73881928+LorisMueller@users.noreply.github.com>
2022-11-22 10:22:38 +0100

    Added entity User

---
+++ src/main/java/ch/zli/m223/model/User.java
@@ -0,0 +1,72 @@
+package ch.zli.m223.model;
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(readOnly = true)
+  private Long id;
+
+  @Column(nullable = false)
+  private String firstname;
+
+  @Column(nullable = false)
+  private String lastname;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  
+
+
+}
